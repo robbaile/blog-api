b2a3200ca7f68a24e2f9e3755e8dfb23fa4ce003
robbaile <robbaile12@outlook.com>
2020-09-07 19:22:23 +0100

    Set View engine

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'jade');
 
 
 app.use('/', indexRouter);
